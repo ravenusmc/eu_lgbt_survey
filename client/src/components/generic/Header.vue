690012ef719365a0f1bc739767328cc95cb2f3a6
ravenusmc <mcuddy77@gmail.com>
2019-12-31 13:42:48 -0500

    working on the header

---
+++ client/src/components/generic/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+
+Header {
+  background: #00AFC9;
+  height: 80px;
+}
+
+</style>
