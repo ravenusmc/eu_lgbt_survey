629f896b541ec862096194be23a5420f2340a2db
ravenusmc <mcuddy77@gmail.com>
2020-01-04 19:48:12 -0500

    working on section one

--- client/src/components/SectionOne.vue
+++ client/src/components/SectionOne.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
+    section one
   </div>
 </template>
 
 <script>
 export default {
   name: 'SectionOne',
-}
+};
 </script>
 
 <style scoped>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="home">
+    <SectionOne/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import SectionOne from '@/components/SectionOne.vue';
 
 export default {
   name: 'home',
+  components: {
+    SectionOne,
+  },
 };
 </script>
