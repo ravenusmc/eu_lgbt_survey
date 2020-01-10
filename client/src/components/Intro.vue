f5464c90905469185ef2d476e9cb7a31de5f4feb
ravenusmc <mcuddy77@gmail.com>
2020-01-10 15:34:51 -0500

    started work on an intro section

---
+++ client/src/components/Intro.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Intro
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Intro',
+};
+</script>
+
+<style scoped>
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
+    <Intro />
     <SectionOne/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Intro from '@/components/Intro.vue';
 import SectionOne from '@/components/SectionOne.vue';
+
 
 export default {
   name: 'home',
   components: {
     SectionOne,
+    Intro,
   },
 };
 </script>
