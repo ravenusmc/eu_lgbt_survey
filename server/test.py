20fe4cb5c12bb2c92e596d244a84f968414076c8
ravenusmc <mcuddy77@gmail.com>
2020-01-10 15:26:09 -0500

    have questions set up for study

--- server/test.py
+++ server/test.py
@@ -15,7 +15,12 @@ class Test():
     def show_Unique_Elements(self):
         print(self.data['question_label'].unique())
 
+    def show_unique_question_answers(self):
+        data = self.data[self.data.question_label == 'In your opinion, how widespread are casual jokes in everyday life about lesbian, gay, bisexual and/or transgender people in the country you live?']
+        print(data['answer'].unique())
+        # death_data_set = self.data[(self.data.FATALITY_YEAR >= yearOne) & (self.data.FATALITY_YEAR <= yearTwo)]
+
 
 test = Test()
 #test.show_Some_Data()()
-test.show_Unique_Elements()
+test.show_unique_question_answers()
