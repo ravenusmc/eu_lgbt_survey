9196377e9fc7631dbaa3f3f8025dbb438967c31f
ravenusmc <mcuddy77@gmail.com>
2020-01-11 16:15:24 -0500

    worked on intro section

--- client/src/components/Intro.vue
+++ client/src/components/Intro.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    Intro
+    <section>
+      <div id='picDiv'>
+      </div>
+      <div>
+        <h1 class='center'>The Study</h1>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -11,4 +17,28 @@ export default {
 </script>
 
 <style scoped>
+
+.center {
+  text-align: center;
+  text-transform: uppercase;
+}
+
+section {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 1em;
+  margin-top: 5%;
+  margin-bottom: 3%;
+}
+
+#picDiv {
+  margin-left: 5%;
+  margin-right: 5%;
+  background-image: url('../assets/img/picOne.jpg');
+  background-size: 100% 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 600px;
+}
+
 </style>
