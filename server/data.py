65cde341855c515b7871eecc91ef8345034ba3af
ravenusmc <mcuddy77@gmail.com>
2020-01-17 11:27:12 -0500

    working a bit more on getting data on server side

--- server/data.py
+++ server/data.py
@@ -11,10 +11,23 @@ class Data():
         self.data = pd.read_csv('./data/LGBT_Survey_DailyLife.csv')
 
     def question_one_data(self, state, orientation):
+        question_one_data = []
+        columns = ['View', 'Count']
+        question_one_data.append(columns)
         data_set = self.data[(self.data.CountryCode == state) & (self.data.subset == orientation)]
         data_set = data_set[(data_set.question_code == 'b1_a')]
-        
-        print(data_set.head())
+        values = ['Very widespread', 'Fairly widespread', 'Fairly rare', 'Very rare', 'Don`t know']
+        for value in values:
+            rows = []
+            loop_data_set = data_set
+            loop_data_set = loop_data_set[(loop_data_set.answer == value)]
+            print(loop_data_set.head())
+            print(loop_data_set['answer'].count())
+            input()
+
+
+
+        print(values)
 
 data = Data()
 data.question_one_data('Austria', 'Lesbian')
