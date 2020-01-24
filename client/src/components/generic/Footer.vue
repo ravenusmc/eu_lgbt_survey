43cb6cf32e79e4dd9fdb2174af0b027e1299f1b7
ravenusmc <mcuddy77@gmail.com>
2020-01-24 12:42:57 -0500

    added in footer

--- client/src/components/generic/Footer.vue
+++ client/src/components/generic/Footer.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <footer>
+      <div class='footer_fix'>
+        <h2>Copyright &copy; 2019 Double Sun Productions<br></h2>
+          <div>
+            <a href="https://www.linkedin.com/in/mcuddy1/"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
+            <a href="https://twitter.com/mcuddy7"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
+            <a href="https://codingsaga.wordpress.com/"><i class="fa fa-pencil fa-3x" aria-hidden="true"></i></a>
+            <a href="https://github.com/ravenusmc"><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a>
+          </div>
+      </div>
     </footer>
   </div>
 </template>
@@ -12,10 +21,27 @@ export default {
 </script>
 
 <style scoped>
+.fa-linkedin-square, .fa-twitter-square, .fa-pencil, .fa-github-square {
+  color: black;
+  margin: 5px;
+}
 
 footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background: #00AFC9;
   height: 400px;
+}
+
+.footer_fix {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 50px;
+  padding-top: 50px;
 }
 
 </style>
